ac8a2b201d22ccdb5c130e86cd7ff6aac5118872
poitre_j <poitre_j@96db4aac-a811-4c53-86d2-a2537d0e056c>
2017-11-03 21:38:17 +0000

    test

--- ImagePanel/poitre_j/imagepanel.php
+++ ImagePanel/poitre_j/imagepanel.php
@@ -6,7 +6,7 @@
 // Login   <poitre_j@etna-alternance.net>
 // 
 // Started on  Fri Nov  3 10:00:19 2017 POITREAU Julien
-// Last update Fri Nov  3 22:03:55 2017 POITREAU Julien
+// Last update Fri Nov  3 22:07:30 2017 POITREAU Julien
 //
 
 function is_url($url)
@@ -158,8 +158,7 @@ while ($counter < $argc - 1)
 	      $image = imagecreatefromjpeg($images[0][$counter1]);
 	    if ($extension['extension'] == "gif")
 	      $image = imagecreatefromgif($images[0][$counter1]);
-	    $img_spec = getimagesize($image);
-	    print_r($img_spec);
+	    $img_spec = getimagesizefromstring($image[0][$counter]);
 	    $new_length = 200;
 	    $reduction = (($new_length * 100) / $img_spec[0]);
 	    $new_heigth = (($img_spec[1] * $reduction) / 100);
