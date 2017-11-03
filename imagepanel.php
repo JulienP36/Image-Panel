dfd09acc3fc966d7413d5d3d4b1b8dc5342b27c2
poitre_j <poitre_j@96db4aac-a811-4c53-86d2-a2537d0e056c>
2017-11-03 14:40:16 +0000

    avancement proj

--- imagepanel.php
+++ imagepanel.php
@@ -6,24 +6,37 @@
 // Login   <poitre_j@etna-alternance.net>
 // 
 // Started on  Fri Nov  3 10:00:19 2017 POITREAU Julien
-// Last update Fri Nov  3 10:43:48 2017 POITREAU Julien
+// Last update Fri Nov  3 14:56:39 2017 POITREAU Julien
 //
 
-$url = $argv[1];
-$url_header = @get_headers($url);
+$option = $argv[1];
 
-if (!$url_header || $url_header[0] == '404')
-  echo "Erreur: nous n'avons pas eu accès à la page ".$argv[1]." Page inexistante ou inatteignable";
-else
+$counter = 1;
+while ($counter < $argc)
   {
-    $content = file_get_contents($argv[1]);
-    preg_match_all("/(?<=src=\")([^\"])+(png|jpg|gif)/",$content, $images);
-    $length = count($images[0]);
-    $counter = 0;
-    while ($counter < $length)
+    $ok = 0;
+    if (file_exists($argv[$counter]) == true)
+      $ok = 1;
+    $url = $argv[$counter];
+    $url_header = @get_headers($url);
+    if ((!$url_header || $url_header[0] == '404') && ($ok == 0))
+      echo "Erreur: Pas de fichier ou d'url valide : ".$argv[$counter]."\n";
+    else
+      $ok = 1;
+    if ($ok == 1)
       {
-	echo $images[0][$counter]."\n";
-	++$counter;
+	$content = file_get_contents($argv[$counter]);
+	preg_match_all("/(?<=src=\")([^\"])+(png|jpg|gif)/",$content, $images);
+	$length = count($images[0]);
+	$counter1 = 0;
+	while ($counter1 < $length)
+	  {
+	    $image = imagecreatefrompng($images[0][$counter1]);
+	    echo $images[0][$counter1]."\n";
+	    ++$counter1;
+	  }
       }
+    ++$counter;
   }
+imagepng($image, "test.png");
 ?>
