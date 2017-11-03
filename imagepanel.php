b8523f9619ea3acbff1cc41766c22bd88f90837a
poitre_j <poitre_j@96db4aac-a811-4c53-86d2-a2537d0e056c>
2017-11-03 17:18:36 +0000

    commit avancement

--- imagepanel.php
+++ imagepanel.php
@@ -6,20 +6,102 @@
 // Login   <poitre_j@etna-alternance.net>
 // 
 // Started on  Fri Nov  3 10:00:19 2017 POITREAU Julien
-// Last update Fri Nov  3 14:56:39 2017 POITREAU Julien
+// Last update Fri Nov  3 18:11:13 2017 POITREAU Julien
 //
 
-$option = $argv[1];
+function is_url($url)
+{
+  $pattern = '|^http(s)?://[a-z0-9]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i';
+  if (preg_match($pattern, $url) > 0)
+    return (true);
+  else
+    return (false);
+}
 
-$counter = 1;
+function option_is_valid($option)
+{
+  $length = strlen($option);
+  $c = 1;
+  while ($c < $length)
+    {
+      if (($option[$c] == 'g') || ($option[$c] == 'j') || ($option[$c] == 'l'))
+	$ok = 1;
+      else if (($option[$c] == 'n') || ($option[$c] == 'N'))
+	$ok = 1;
+      else if (($option[$c] == 's') || ($option[$c] == 'p'))
+	$ok = 1;
+      else
+	return (false);
+      ++$c;
+    }
+  return (true);
+}
+$g = 0;
+$j = 0;
+$l = 0;
+$n = 0;
+$N = 0;
+$p = 0;
+$s = 0;
+$validate = 1;
+
+$option = $argv[1];
+if ($option[0] == '-')
+  {
+    if (option_is_valid($option))
+      {
+	echo "Option(s) valide(s)\n";
+	$length = strlen($option);
+	$cntr = 1;
+	while ($cntr < $length)
+	  {
+	    if ($option[$cntr] == 'g')
+	      $g = 1;
+	    else if ($option[$cntr] == 'j')
+	      $j = 1;
+	    else if ($option[$cntr] == 'l')
+	      $l = 1;
+	    else if ($option[$cntr] == 'n')
+	      $n = 1;
+	    else if ($option[$cntr] == 'N')
+	      $N = 1;
+	    else if ($option[$cntr] == 'p')
+	      $p = 1;
+	    else if ($option[$cntr] == 's')
+	      $s = 1;
+	    ++$cntr;
+	  }
+      }
+    else
+      {
+	$validate = 0;
+	echo "Options(s) invalide(s)\n";
+      }
+$counter = 2;
+}
+else
+  {
+    echo "Pas d'options\n";
+    $counter = 1;
+  }
+echo "g:".$g." j:".$j." l:".$l." n:".$n." N:".$N." p:".$p." s:".$s."\n";
+if ($validate == 1)
 while ($counter < $argc)
   {
     $ok = 0;
     if (file_exists($argv[$counter]) == true)
       $ok = 1;
     $url = $argv[$counter];
-    $url_header = @get_headers($url);
-    if ((!$url_header || $url_header[0] == '404') && ($ok == 0))
+    if (is_url($url))
+      {
+	$url_headr = get_headers($url);
+      }
+    else
+      {
+	$ok = 0;
+	$url_headr[0] = "404";
+      }
+    if (($url_headr[0] == "404") && ($ok == 0))
       echo "Erreur: Pas de fichier ou d'url valide : ".$argv[$counter]."\n";
     else
       $ok = 1;
@@ -29,7 +111,7 @@ while ($counter < $argc)
 	preg_match_all("/(?<=src=\")([^\"])+(png|jpg|gif)/",$content, $images);
 	$length = count($images[0]);
 	$counter1 = 0;
-	while ($counter1 < $length)
+	while ($counter1 < $length - 1)
 	  {
 	    $image = imagecreatefrompng($images[0][$counter1]);
 	    echo $images[0][$counter1]."\n";
@@ -38,5 +120,10 @@ while ($counter < $argc)
       }
     ++$counter;
   }
-imagepng($image, "test.png");
+if ($g == 1)
+  imagegif($image, "test.gif");
+if ($j == 1)
+  imagejpeg($image, "test.jpeg");
+if ($p == 1)
+  imagepng($image, "test.png");
 ?>
